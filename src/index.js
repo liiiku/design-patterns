0f57f6ba151aaa042617514538ba06c40d878e92
ruinan.li02 <ruinan.li02@ele.me>
2018-08-01 17:44:46 +0800

    feat:停车场问题

--- src/index.js
+++ src/index.js
@@ -1,43 +1,126 @@
-// 车 父类
+// 车辆
 class Car {
-  constructor(number, name) {
-    this.number = number
-    this.name = name
+  constructor(num) {
+    this.num = num
   }
 }
 
-// 快车 Kuaiche
-class Kuaiche extends Car {
-  constructor(number, name) {
-    super(number, name)
-    this.price = 1
+// 摄像头
+class Camera {
+  shot(car) {
+    return {
+      num: car.num,
+      inTime: Date.now() // 驶入时间
+    }
   }
 }
 
-// 专车 Zhuanche
-class Zhuanche extends Car {
-  constructor(number, name) {
-    super(number, name)
-    this.price = 2
+// 出口显示屏
+class Screen {
+  show(car, inTime) {
+    console.log('车牌号', car.num)
+    console.log('停车时间', Date.now() - inTime)
   }
 }
 
+// 停车场
+class Park {
+  constructor(floors) {
+    this.floors = floors || []
+    this.camera = new Camera()
+    this.screen = new Screen()
+    this.carList = {} // 存储摄像头拍摄返回的车辆信息
+  }
+  in(car) {
+    // 通过摄像头获取信息
+    const info = this.camera.shot(car)
+    // 停到某个停车位
+    const i = parseInt(Math.random() * 100 % 100)
+    const place = this.floors[0].places[i]
+    place.in()
+    info.place = place
+    // 记录信息
+    this.carList[car.num] = info
+  }
+  out(car) {
+    // 获取信息
+    const info = this.carList[car.num]
+    // 将停车位清空
+    const place = info.place
+    place.out()
+    // 显示时间
+    this.screen.show(car, info.inTime)
+    // 清空记录
+    delete this.carList[car.num]
+  }
+  // 整个停车场的空余数量
+  emptyNum() {
+    return this.floors.map(floor => {
+      return `${floor.index} 层还有 ${floor.emptyPlaceNum()} 个空余车位`
+    }).join('\n')
+  }
+}
 
-// 行程
-class Trip {
-  constructor(car) {
-    this.car = car
+// 层
+class Floor {
+  constructor(index, places) {
+    this.index = index
+    this.places = places || []
   }
-  start() {
-    console.log(`行程开始，名称：${this.car.name}，车牌号：${this.car.name}`)
+  // 这一层有多少个空余车位
+  emptyPlaceNum() {
+    let num = 0
+    this.places.forEach(p => {
+      if (p.empty) {
+        num = num + 1
+      }
+    })
+    return num
   }
-  end() {
-    console.log('行程结束，金额：' + this.car.price * 5)
+}
+
+// 车位
+class Place {
+  constructor() {
+    this.empty = true
+  }
+  in() {
+    this.empty = false
+  }
+  out() {
+    this.empty = true
   }
 }
 
 // 测试
-let car = new Kuaiche(100, '大众汽车')
-let trip = new Trip(car)
-trip.start()
-trip.end()
+// 初始化停车场
+const floors = []
+for (let i = 0; i < 3; i++) {
+  const places = []
+  for (let j = 0; j < 100; j++) {
+    places[j] = new Place()
+  }
+  floors[i] = new Floor(i+1, places)
+}
+const park = new Park(floors)
+
+// 初始化车辆
+const car1 = new Car(100)
+const car2 = new Car(200)
+const car3 = new Car(300)
+
+console.log('第一辆车进入')
+console.log(park.emptyNum())
+park.in(car1)
+console.log('第二辆车进入')
+console.log(park.emptyNum())
+park.in(car2)
+console.log('第一辆车离开')
+park.out(car1)
+console.log('第二辆车离开')
+park.out(car2)
+console.log('第三辆车进入')
+console.log(park.emptyNum())
+park.in(car3)
+console.log('第三辆车离开')
+park.out(car3)
