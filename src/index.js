87b38eb1025a7f8d14db6ff30d748f3aee0e52d5
ruinan.li02 <ruinan.li02@ele.me>
2018-08-02 17:50:49 +0800

    feat:工厂模式

--- src/index.js
+++ src/index.js
@@ -1,126 +1,37 @@
-// 车辆
-class Car {
-  constructor(num) {
-    this.num = num
+class Product {
+  constructor(name) {
+    this.name = name
+  }
+  init() {
+    console.log('init', this.name)
+  }
+  fn1() {
+    console.log('fn1')
+  }
+  fn2() {
+    console.log('fn2')
   }
 }
 
-// 摄像头
-class Camera {
-  shot(car) {
-    return {
-      num: car.num,
-      inTime: Date.now() // 驶入时间
-    }
-  }
-}
+// 工厂
+// class Creator {
+//   create(name) {
+//     return new Product(name)
+//   }
+// }
 
-// 出口显示屏
-class Screen {
-  show(car, inTime) {
-    console.log('车牌号', car.num)
-    console.log('停车时间', Date.now() - inTime)
-  }
-}
-
-// 停车场
-class Park {
-  constructor(floors) {
-    this.floors = floors || []
-    this.camera = new Camera()
-    this.screen = new Screen()
-    this.carList = {} // 存储摄像头拍摄返回的车辆信息
-  }
-  in(car) {
-    // 通过摄像头获取信息
-    const info = this.camera.shot(car)
-    // 停到某个停车位
-    const i = parseInt(Math.random() * 100 % 100)
-    const place = this.floors[0].places[i]
-    place.in()
-    info.place = place
-    // 记录信息
-    this.carList[car.num] = info
-  }
-  out(car) {
-    // 获取信息
-    const info = this.carList[car.num]
-    // 将停车位清空
-    const place = info.place
-    place.out()
-    // 显示时间
-    this.screen.show(car, info.inTime)
-    // 清空记录
-    delete this.carList[car.num]
-  }
-  // 整个停车场的空余数量
-  emptyNum() {
-    return this.floors.map(floor => {
-      return `${floor.index} 层还有 ${floor.emptyPlaceNum()} 个空余车位`
-    }).join('\n')
-  }
-}
-
-// 层
-class Floor {
-  constructor(index, places) {
-    this.index = index
-    this.places = places || []
-  }
-  // 这一层有多少个空余车位
-  emptyPlaceNum() {
-    let num = 0
-    this.places.forEach(p => {
-      if (p.empty) {
-        num = num + 1
-      }
-    })
-    return num
-  }
-}
-
-// 车位
-class Place {
-  constructor() {
-    this.empty = true
-  }
-  in() {
-    this.empty = false
-  }
-  out() {
-    this.empty = true
-  }
+let OP = function(name) {
+  return new Product(name)
 }
 
 // 测试
-// 初始化停车场
-const floors = []
-for (let i = 0; i < 3; i++) {
-  const places = []
-  for (let j = 0; j < 100; j++) {
-    places[j] = new Place()
-  }
-  floors[i] = new Floor(i+1, places)
-}
-const park = new Park(floors)
+// let creator = new Creator()
+// let p = creator.create('p1')
+// p.init()
+// p.fn1()
+// p.fn2()
 
-// 初始化车辆
-const car1 = new Car(100)
-const car2 = new Car(200)
-const car3 = new Car(300)
-
-console.log('第一辆车进入')
-console.log(park.emptyNum())
-park.in(car1)
-console.log('第二辆车进入')
-console.log(park.emptyNum())
-park.in(car2)
-console.log('第一辆车离开')
-park.out(car1)
-console.log('第二辆车离开')
-park.out(car2)
-console.log('第三辆车进入')
-console.log(park.emptyNum())
-park.in(car3)
-console.log('第三辆车离开')
-park.out(car3)
+let op = OP('p1')
+op.init()
+op.fn1()
+op.fn2()
